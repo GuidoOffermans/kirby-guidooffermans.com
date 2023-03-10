647815ae796429914d2fbe2758c7a44026e2d67b
Guido Offermans <guidooffermans@hotmail.com>
2023-03-10 22:52:22 +0100

    edit panel config

--- site/config/config.php
+++ site/config/config.php
@@ -2,12 +2,13 @@
 
 return [
 	//	Panel
-	'install' => true,
-	'slug'    => env('KIRBY_PANEL_SLUG', 'panel'),
-
+	'panel'  => [
+		'install' => env('KIRBY_PANEL_INSTALL', false),
+		'slug'    => env('KIRBY_PANEL_SLUG', 'panel'),
+	],
 	//	Debug
-	'debug'   => env('KIRBY_DEBUG', false),
+	'debug'  => env('KIRBY_DEBUG', false),
 
 	//	Blocks
-	'blocks'  => require_once 'blocks.php'
+	'blocks' => require_once 'blocks.php'
 ];
